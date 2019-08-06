1b25dc3a2fb3bd8a9278b20052c79ab7566a941a
Jason Santana <jsantana006@gmail.com>
2019-08-06 21:49:17 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
 };
 
 
-function updatePlaylist(playlist, artistName, songTitle){
+var updatePlaylist = ({}, playlist, artistName, songTitle){
   playlist.assign(artistName, songTitle);
 }
 
